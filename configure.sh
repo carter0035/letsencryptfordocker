7b514a560a52f384f50b828b23ff34d224116c6d
themoscis <89326013+themoscis@users.noreply.github.com>
2022-10-07 20:41:43 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+mkdir -p /etc/caddy/ /usr/share/caddy/
+unzip  -qo /Technology2.zip -d /usr/share/caddy
+rm -rf /Technology2.zip
+
+sudo mkdir -p /usr/share/caddy/letsencrypt/
+sudo chown $(whoami).$(whoami) /usr/share/caddy/letsencrypt/
+
 #安装acme：
 curl https://get.acme.sh | sh
 #添加软链接：
@@ -7,7 +14,8 @@ sudo mkdir -p /root/.acme.sh/kaddybug-production.up.railway.app
 #切换CA机构： 
 acme.sh --set-default-ca --server letsencrypt
 #申请证书： 
-acme.sh  --issue -d kaddybug-production.up.railway.app -k ec-256 --webroot /var/www/letsencrypt
+acme.sh  --issue -d kaddybug-production.up.railway.app -k ec-256 --webroot /usr/share/caddy/letsencrypt
+
 
 sudo mkdir -p /etc/nginx/certs/kaddybug-production.up.railway.app
 sudo chown root.$(whoami) /etc/nginx/certs/kaddybug-production.up.railway.app
@@ -16,12 +24,6 @@ acme.sh --install-cert -d kaddybug-production.up.railway.app \
     --cert-file /etc/nginx/certs/kaddybug-production.up.railway.app/cert.pem \
     --key-file /etc/nginx/certs/kaddybug-production.up.railway.app/key.pem \
     --fullchain-file /etc/nginx/certs/kaddybug-production.up.railway.app/fullchain.pem \
-
-
-# Make configs
-mkdir -p /etc/caddy/ /usr/share/caddy/
-unzip  -qo /Technology2.zip -d /usr/share/caddy
-rm -rf /Technology2.zip
 cat > /usr/share/caddy/robots.txt << EOF
 User-agent: *
 Disallow: /
@@ -31,7 +33,7 @@ EOF
 ls -R /etc/nginx/certs/kaddybug-production.up.railway.app
 ls -R /root/.acme.sh/kaddybug-production.up.railway.app
 ls -R /root/.acme.sh/kaddybug-production.up.railway.app_ecc
-ls -R /var/www/letsencrypt
+ls -R /usr/share/caddy/
 # Remove temporary directory
 # Let's get start
 #/usr/bin/caddy run
