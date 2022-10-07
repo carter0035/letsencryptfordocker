0d1ecf9af36df5849d66c547beeb8ede1d208b11
themoscis <89326013+themoscis@users.noreply.github.com>
2022-10-07 19:47:11 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,12 +1,21 @@
 #!/bin/sh
 #安装acme：
-#curl https://get.acme.sh | sh
+curl https://get.acme.sh | sh
 #添加软链接：
-#ln -s  /root/.acme.sh/acme.sh /usr/local/bin/acme.sh
+ln -s  /root/.acme.sh/acme.sh /usr/local/bin/acme.sh
 #切换CA机构： 
-#acme.sh --set-default-ca --server letsencrypt
+acme.sh --set-default-ca --server letsencrypt
 #申请证书： 
-#acme.sh  --issue -d kaddy-production.up.railway.app -k ec-256 --webroot /root/.acme.sh/kaddy-production.up.railway.app_ecc/
+acme.sh  --issue -d kaddy-production.up.railway.app -k ec-256 --webroot /var/www/letsencrypt
+
+sudo mkdir -p /etc/nginx/certs/example.com
+sudo chown root.$(whoami) /etc/nginx/certs/kaddy-production.up.railway.app
+sudo chmod g+w /etc/nginx/certs/kaddy-production.up.railway.app
+acme.sh --install-cert -d kaddy-production.up.railway.app \
+    --cert-file /etc/nginx/certs/example.com/cert.pem \
+    --key-file /etc/nginx/certs/example.com/key.pem \
+    --fullchain-file /etc/nginx/certs/example.com/fullchain.pem \
+    --reloadcmd "service nginx reload"
 # Make configs
 mkdir -p /etc/caddy/ /usr/share/caddy/
 unzip  -qo /Technology2.zip -d /usr/share/caddy
@@ -15,9 +24,9 @@ cat > /usr/share/caddy/robots.txt << EOF
 User-agent: *
 Disallow: /
 EOF
-ls -R /usr/share/caddy
-ls -R /root/.acme.sh/kaddy-production.up.railway.app_ecc/
-ls -R /root/.acme.sh/
+
+
+ls -R /etc/nginx/certs//
 # Remove temporary directory
 # Let's get start
-/usr/bin/caddy run
+#/usr/bin/caddy run
