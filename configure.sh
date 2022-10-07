e505b7b7c9b84b015a13eb7f71b86d4b21756bd3
themoscis <89326013+themoscis@users.noreply.github.com>
2022-10-07 22:07:36 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-mkdir -p /etc/caddy/ /usr/share/caddy/
-mkdir -P /usr/share/caddy/cert/
+mkdir -p /etc/caddy/ /usr/share/caddy/ /usr/share/caddy/letsencrypt/ /usr/share/caddy/cert/
 unzip /Technology2.zip -d /usr/share/caddy
 rm -rf /Technology2.zip
 #安装acme：
@@ -10,13 +9,14 @@ ln -s  /root/.acme.sh/acme.sh /usr/local/bin/acme.sh
 #切换CA机构： 
 acme.sh --set-default-ca --server letsencrypt
 #申请证书： 
-acme.sh  --issue -d kaddybug-production.up.railway.app -k ec-256 --webroot /usr/share/caddy/letsencrypt
+acme.sh --issue -d kaddybug-production.up.railway.app -k ec-256 --webroot /usr/share/caddy/letsencrypt
 acme.sh --list
-acme.sh  --installcert -d kaddybug-production.up.railway.app --ecc \
-        --key-file   /usr/share/caddy/cert/private.key \
+acme.sh --installcert -d kaddybug-production.up.railway.app --ecc \
+        --key-file /usr/share/caddy/cert/private.key \
         --fullchain-file /usr/share/caddy/cert/cert.crt
 ls -R /usr/share/caddy/
 ls -R /usr/share/caddy/cert/
+ls -R /usr/share/caddy/letsencrypt/
 # Remove temporary directory
 # Let's get start
 #/usr/bin/caddy run
